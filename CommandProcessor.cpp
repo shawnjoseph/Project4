5c8ca0703bac1dc72ee629d8c104f2335aa08b8a
shawnjoseph <shawnjoseph@mail.usf.edu>
2016-07-18 09:54:34 -0400

    "updated CommandProcessor.h"

--- CommandProcessor.cpp
+++ CommandProcessor.cpp
@@ -43,9 +43,9 @@ void CommandProcessor::Process_Commands() {
 
 void CommandProcessor::Process_Initial_Cmd(const string &cmd) {
     if (cmd == "DirectedGraph") {
-        command_state = DGraph;
+        command_state = DirectedGraph;
     } else if (cmd == "UndirectedGraph") {
-        command_state = UGraph;
+        command_state = UndirectedGraph;
     } else {
         cout << "Quit command\n";
         command_state = Done;
@@ -55,36 +55,67 @@ void CommandProcessor::Process_Initial_Cmd(const string &cmd) {
 
 void CommandProcessor::ProcessDirectedGraph(const string &cmd) {
     if (cmd == "Insert") {
-        // Fill part in with getting info
-        diGraph->insert(node, parentKey);
+        string u, v;
+        double weight;
+        cout << "Enter name of start vertex: ";
+        getline(cin, u);
+        cout << "Enter name of end vertex: ";
+        getline(cin, v);
+        cout << "Enter weight of edge between vertices: ";
+        cin >> weight;
+        getchar();
+        diGraph->insert(u, v, weight);
+        cout << "Edge added" << endl;
     } else if (cmd == "Build Graph") {
         diGraph->buildTree();
+        cout << "The graph has been built." << endl;
     } else if (cmd == "Clear") {
         diGraph->clear();
     } else if (cmd == "Reset") {
-        diGraph->buildTree();
+        diGraph->reset();
+        cout << "Vertices have been reset." << endl;
     } else if (cmd == "Delete") {
-        string data;
-        cout << "What data does the TreeNode you want to delete have?" << endl;
-        getline(cin, data);
-        TreeNode <string> *node = new TreeNode<string>("General", data);
-        diGraph->del(node);
+        string v;
+        cout << "Enter vertex to be deleted." << endl;
+        getline(cin, v);
+        diGraph->del(v);
     } else if (cmd == "Indegree") {
-        diGraph->indegree();
+        string v;
+        cout << "Enter vertex name: ";
+        getline(cin, v);
+        cout << "Indegree of " << v << " is: " << diGraph->indegree(v) << endl;
     } else if (cmd == "Outdegree") {
-
+        string v;
+        cout << "Enter vertex name: ";
+        getline(cin, v);
+        cout << "Outdegree of " << v << " is: " << diGraph->outdegree(v) << endl;
     } else if (cmd == "Edgecount") {
-
+        cout << "There are " << diGraph->numEdges << " edges in the graph." << endl;
     } else if (cmd == "Adjacent") {
-
+        string u, v;
+        cout << "Enter name of start vertex: ";
+        getline(cin, u);
+        cout << "Enter name of end vertex: ";
+        getline(cin, v);
+        diGraph->adjacent(u, v);
     } else if (cmd == "Depth First Search") {
-
+        diGraph->DFS();
     } else if (cmd == "Breadth First Search") {
-
+        diGraph->BFS();
     } else if (cmd == "Distance") {
-
+        string u, v;
+        cout << "Enter name of start vertex: ";
+        getline(cin, u);
+        cout << "Enter name of end vertex: ";
+        getline(cin, v);
+        diGraph->distance(u, v);
     } else if (cmd == "Short Path") {
-
+        string u, v;
+        cout << "Enter name of start vertex: ";
+        getline(cin, u);
+        cout << "Enter name of end vertex: ";
+        getline(cin, v);
+        diGraph->shortPath(u, v);
     } else if (cmd == "Is Empty") {
         diGraph->empty();
     } else if (cmd == "Exit") {
@@ -95,65 +126,76 @@ void CommandProcessor::ProcessDirectedGraph(const string &cmd) {
 }
 
 void CommandProcessor::ProcessUndirectedGraph(const string &cmd) {
-    if (cmd == "Max") {
-        cout << "Max: " << "Key[" << graph->getMax()->getKey() << "]"
-        << " Value[" << graph->getMax()->getValue() << "]" << endl;
-    } else if (cmd == "Size") {
-        cout << "Size: " << graph->getSize() << endl;
-    } else if (cmd == "Height") {
-        cout << "Height: " << graph->getHeight() << endl;
+    if (cmd == "Build Graph") {
+        graph->buildGraph();
+        cout << "The graph has been built." << endl;
     } else if (cmd == "Empty") {
-        if (graph->empty()) {
-            cout << "Heap is empty." << endl;
+        graph->empty();
+    } else if (cmd == "Insert") {
+        string u, v;
+        double weight;
+        cout << "Enter name of start vertex: ";
+        getline(cin, u);
+        cout << "Enter name of end vertex: ";
+        getline(cin, v);
+        cout << "Enter weight of edge between vertices: ";
+        cin >> weight;
+        getchar();
+        graph->insert(u, v, weight);
+        cout << "Edge added" << endl;
+    } else if (cmd == "Check Adjacency") {
+        string u, v;
+        cout << "Enter name of start vertex: ";
+        getline(cin, u);
+        cout << "Enter name of end vertex: ";
+        getline(cin, v);
+        graph->adjacent(u, v);
+    } else if (cmd == "Find Degree") {
+        string v;
+        cout << "Enter name of vertex: " << endl;
+        getline(cin, v);
+        cout << "Degree of " << v << " is " << graph->degree(v) << endl;
+    } else if (cmd == "Edge Count") {
+        cout << "There are " << graph->numEdges << " edges in the graph." << endl;
+    } else if (cmd == "Check Connection") {
+        if (graph->isConnected()) {
+            cout << "The graph is connected." << endl;
+        } else {
+            cout << "The graph is not connected." << endl;
         }
-        else {
-            cout << "Heap is Not Empty." << endl;
-        }
-    } else if (cmd == "Leaves") {
-        cout << "Number of Leaves: " << graph->leaves() << endl;
-    } else if (cmd == "Build Tree") {
-        graph->buildTree();
     } else if (cmd == "Clear") {
         graph->clear();
-    } else if (cmd == "Insert") {
-        int key;
-        string value;
-        cout << "Enter key: ";
-        cin >> key;
-        while (cin.fail()) {
-            cout << "Error" << std::endl;
-            cin.clear();
-            cin.ignore(256, '\n');
-            cout << "Enter key: ";
-            cin >> key;
-        }
-        getchar();
-        cout << "Enter value: ";
-        getline(cin, value);
-        graph->insert(key, value);
-    } else if (cmd == "Delete Max") {
-        int max = graph->getMax()->getKey();
-        graph->delMax();
-        cout << "Max " << max << " deleted." << endl;
+    } else if (cmd == "Reset Vertices") {
+        graph->reset();
+        cout << "Vertices have been reset." << endl;
+    } else if (cmd == "Delete Vertex") {
+        string v;
+        cout << "Enter vertex to be deleted." << endl;
+        getline(cin, v);
+        graph->del(v);
     } else if (cmd == "Display") {
-        graph->Display();
+        graph->display();
+    } else if (cmd == "Breadth-First Search") {
+        graph->BFS();
+    } else if (cmd == "Depth-First Search") {
+        graph->DFS();
+    } else if (cmd == "Find Minimum-Spanning Tree") {
     } else if (cmd == "Exit") {
         exit(0);
     } else {
-        cout << "Error" << endl;
+        cerr << "Error" << endl;
     }
 }
 
 void CommandProcessor::Create_Menus() {
     // Menu for Initial command state
-    Menu *menu = new Menu("Which Tree do you want to create?");
-    menu->Add_Command("Tree");
-    menu->Add_Command("Heap");
-    menu->Add_Command("AVLTree");
+    Menu *menu = new Menu("Which Graph would you like to create?");
+    menu->Add_Command("DirectedGraph");
+    menu->Add_Command("UndirectedGraph");
 
     menus[Initial] = menu;
 
-    //Menu for Tree
+    //Menu for DirectedGraph
     menu = new Menu("Enter command number:\n");
     menu->Add_Command("Get Root");
     menu->Add_Command("Get Size");
@@ -173,41 +215,25 @@ void CommandProcessor::Create_Menus() {
     menu->Add_Command("Insert");
     menu->Add_Command("Delete");
     menu->Add_Command("Exit");
-    menus[_Tree] = menu;
+    menus[DirectedGraph] = menu;
 
-    //Menu for Heap
+    //Menu for UndirectedGraph
     menu = new Menu("Enter command number:\n");
-    menu->Add_Command("Max");
-    menu->Add_Command("Size");
-    menu->Add_Command("Height");
+    menu->Add_Command("Build Graph");
     menu->Add_Command("Empty");
-    menu->Add_Command("Leaves");
-    menu->Add_Command("Build Tree");
-    menu->Add_Command("Clear");
     menu->Add_Command("Insert");
-    menu->Add_Command("Delete Max");
+    menu->Add_Command("Check Adjacency");
+    menu->Add_Command("Find Degree");
+    menu->Add_Command("Edge Count");
+    menu->Add_Command("Check Connection");
+    menu->Add_Command("Clear");
+    menu->Add_Command("Reset Vertices");
+    menu->Add_Command("Delete Vertex");
     menu->Add_Command("Display");
+    menu->Add_Command("Breadth-First Search");
+    menu->Add_Command("Depth-First Search");
+    menu->Add_Command("Find Minimum-Spanning Tree");
     menu->Add_Command("Exit");
-    menus[_Heap] = menu;
+    menus[UndirectedGraph] = menu;
 
-    //Menu for AVLTree
-    menu = new Menu("Enter command number:\n");
-    menu->Add_Command("Get Root");
-    menu->Add_Command("Get Size");
-    menu->Add_Command("Get Height");
-    menu->Add_Command("Get Depth");
-    menu->Add_Command("Is Empty");
-    menu->Add_Command("Get Leaves");
-    menu->Add_Command("Get Siblings");
-    menu->Add_Command("Find Tree Node");
-    menu->Add_Command("Preorder");
-    menu->Add_Command("Postorder");
-    menu->Add_Command("Levelorder");
-    menu->Add_Command("Inorder");
-    menu->Add_Command("Build Tree");
-    menu->Add_Command("Clear");
-    menu->Add_Command("Insert");
-    menu->Add_Command("Delete");
-    menu->Add_Command("Exit");
-    menus[_AVLTree] = menu;
 }
